06e45202f5d053914baf54347260fcba8cf85575
MHLRGB <101468397+MHLRGB@users.noreply.github.com>
2024-03-28 22:37:30 +0900

    LoginSuccessHandler added

--- src/main/frontend/src/common.js
+++ src/main/frontend/src/common.js
@@ -2,7 +2,26 @@ import React, {useEffect, useState} from 'react';
 
 
 const Common = () => {
+    const [data, setData] = useState('');
 
+    const sendData = async () => {
+        try {
+          const response = await fetch('/common', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ yourData: data }),
+          });
+          
+          // 서버로부터 응답을 처리하거나 확인합니다.
+          const responseData = await response.json();
+          console.log(responseData);
+        } catch (error) {
+          console.error('Error:', error);
+        }
+      };
+      
     return (
         <>
       <input
